3be1c682dbfdef91229f36340284ea604778e5d8
St-Chill <74183910+St-Chill@users.noreply.github.com>
2020-11-30 12:12:15 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,14 +1,14 @@
 <?php
 if (isset($_POST['submit'])) {
-    $name= $_POST['name'];
-    $subject= $_POST['subject'];
-    $mailfrom= $_POST['mail'];
-    $message= $_POST['message'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailfrom = $_POST['mail'];
+    $message = $_POST['message'];
 
-$mailto = "ilyaas.kahn@younglings.africa"
-$headers = "From: ".$mailfrom;
+$mailTo = "ilyaas.kahn@younglings.africa"
+$headers = "From: ".$mailFrom;
 $txt= "You have recieved an e-mail from ".$name.".\n\n".$message;
-mail($mailto, $subject, $text, $headers);
-header("location: index.php?mailsend");
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
 }
 ?>
